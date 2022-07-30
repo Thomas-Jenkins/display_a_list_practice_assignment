0ebbb41bdeb43adf5643c6d6e813242535a60521
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-07-29 17:18:36 -0700

    complete 4th list with array within an array.

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ import { beers } from './beer-data.js';
 import { renderBeer } from './render-beer.js';
 import { restaurants } from './restaurants-data.js';
 import { renderRestaurants } from './render-resturants.js';
+import { games } from './games-data.js';
+import { renderGames } from './render-games.js';
 
 const candyItemListEl = document.getElementById('candies');
 const beerId = document.getElementById('beers');
 const restaurantItemListEl = document.getElementById('restaurants');
+const gameItemListEl = document.getElementById('games');
 
 // for (let candy of candies) {
         
@@ -31,6 +34,10 @@ for (let candy of candies) {
 for (let restaurantItem of restaurants) {
     const restaurantsEl = renderRestaurants(restaurantItem);
     restaurantItemListEl.append(restaurantsEl);
+}
+for (let game of games) {
+    const gameEl = renderGames(game);
+    gameItemListEl.append(gameEl);
 }
 
 
---
+++ games-data.js
@@ -0,0 +1,22 @@
+export const games = [
+    {
+        title: 'Total War, Rome 2',
+        genre: ['Strategy', 'Historical', 'Turn-Based'],
+        hoursplayed: '370 Hours'
+    },
+    {
+        title: 'Factorio',
+        genre: ['Automation', 'Base Building', 'survival'],
+        hoursplayed: '420 Hours'
+    },
+    {
+        title: 'Rimworld',
+        genre: ['Colony Sim', 'Base Building', 'Survival'],
+        hoursplayed: '1,711 hours *help me*'
+    },
+    {
+        title: 'Civilization VI',
+        genre: ['Turned-Based Strategy', 'Historical', 'simulation'],
+        hoursplayed: '109 Hours'
+    }
+];
